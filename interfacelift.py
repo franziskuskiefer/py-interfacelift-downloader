affde67b4e9c5be3446baa15e8841eba6b7ad201
Øystein Walle <oystwa@gmail.com>
2011-05-26 13:31:59 +0200

    Expermient. Only one wget command is executed. This enables the easy use of --random-wait etc.

--- interfacelift.py
+++ interfacelift.py
@@ -4,20 +4,20 @@ import urllib2
 import re
 
 url       = 'http://interfacelift.com/wallpaper/downloads/date/hdtv/1080p/'
-directory = '/datapool/public/wallpaper/1080p'
+directory = '~'
 useragent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)'
 pattern   = '(?<=<a href=")/wallpaper/.*jpg(?=">)' # The regex pattern used. Check the README
+wgetoptions = " --wait=5 --random-wait --limit-rate=100K --retry-connrefused --referer=http://interfacelift.com/ "
 
+pictures = []
 count     = 1
-while count < 999999:
-        data       = urllib2.urlopen(url + "index" + str(count) + ".html").read()
-        pictures   = re.findall(pattern, data)
-        urlcount   = len(pictures)
-        for picture in pictures:
-                if os.path.exists(directory + picture):
-                        print 'Directory up to date. Found existing file.'
-                        quit()
-                os.system('wget -P ' + directory + ' -nc -U "' + useragent + '" ' + 'http://interfacelift.com' + picture)
-        if urlcount == 0:
-                quit()
+while count < 2:
+        data = urllib2.urlopen(url + "index" + str(count) + ".html").read()
+        pictures += re.findall(pattern, data)
         count += 1
+if len(pictures) > 0:
+        print "Attempting to download", len(pictures), "files with wget..."
+        os.system('wget -P ' + directory + ' -nc -U "' + useragent + '" ' + wgetoptions + ' -- ' + " http://interfacelift.com".join(pictures))
+        print "Complete."
+else:
+        print "No pictures to download"
