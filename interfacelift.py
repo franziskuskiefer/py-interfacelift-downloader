a41631edf9d80191e91010fcc22f264c23f95b13
Øystein Walle <oystwa@gmail.com>
2011-06-03 23:34:30 +0200

    Markdownized the readme

--- interfacelift.py
+++ interfacelift.py
@@ -10,7 +10,7 @@ urlspec   = 'rating/widescreen/1920x1200/'
 directory = expanduser('~')+'/Pictures/Interfacelift'
 
 urlbase   = 'http://interfacelift.com/wallpaper/'
-pattern   = '(?<=<a href="/wallpaper/).*jpg(?=">)'
+pattern   = '(?<=<a href="/wallpaper/).*\.jpg(?=">)'
 useragent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)'
 url       = urlbase+'downloads/'+urlspec
 wgetcmd   = [ 'wget', '-U', useragent, '-P', directory, '-nc', '--wait=5', '--random-wait', '--limit-rate=100K', '--retry-connrefused', '--referer=http://www.interfacelift.com/wallpaper/' ]
