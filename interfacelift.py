5d2fcc7378f922d7372fec48ad93f3101ab0db61
Franziskus Kiefer <franziskuskiefer@gmail.com>
2011-07-02 09:57:34 +0200

    added CTRL+C to end downloading

--- interfacelift.py
+++ interfacelift.py
@@ -2,11 +2,20 @@
 import os
 import urllib2
 import re
+import signal
+import sys
 
 url       = 'http://interfacelift.com/wallpaper/downloads/date/hdtv/1080p/'
 directory = '/datapool/public/wallpaper/1080p'
 useragent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)'
 pattern   = '(?<=<a href=")/wallpaper/.*jpg(?=">)' # The regex pattern used. Check the README
+
+def signal_handler(signal, frame):
+        print 'You pressed Ctrl+C!'
+        sys.exit(0)
+
+# add event handler to catch CTRL+C event and abort downloading
+signal.signal(signal.SIGINT, signal_handler)
 
 count     = 1
 while count < 999999:
