00a383152d546dd9a5c51f7dfcf8a98a4fbe1287
Franziskus Kiefer <franziskuskiefer@gmail.com>
2012-06-17 10:18:00 +0200

    added command line argument for path and write url into image

--- interfacelift.py
+++ interfacelift.py
@@ -6,7 +6,6 @@ import signal
 import sys
 
 url       = 'http://interfacelift.com/wallpaper/downloads/date/hdtv/1080p/'
-directory = '/datapool/public/wallpaper/1080p'
 useragent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)'
 pattern   = '(?<=<a href=")/wallpaper/.*jpg(?=">)' # The regex pattern used. Check the README
 
@@ -17,16 +16,25 @@ def signal_handler(signal, frame):
 # add event handler to catch CTRL+C event and abort downloading
 signal.signal(signal.SIGINT, signal_handler)
 
-count     = 1
-while count < 999999:
-        data       = urllib2.urlopen(url + "index" + str(count) + ".html").read()
-        pictures   = re.findall(pattern, data)
-        urlcount   = len(pictures)
-        for picture in pictures:
-                if os.path.exists(directory + picture):
-                        print 'Directory up to date. Found existing file.'
-                        quit()
-                os.system('wget -P ' + directory + ' -nc -U "' + useragent + '" ' + 'http://interfacelift.com' + picture)
-        if urlcount == 0:
-                quit()
-        count += 1
+if len(sys.argv) > 1:
+    directory = sys.argv[1]
+    count     = 1
+    while count < 999999:
+            data       = urllib2.urlopen(url + "index" + str(count) + ".html").read()
+            pictures   = re.findall(pattern, data)
+            urlcount   = len(pictures)
+            for picture in pictures:
+                    s = picture.replace('_', '/').split('/')
+                    if os.path.exists(directory+s[3]+"_"+s[4]+"_"+s[5]):
+                            print 'Found file already...'
+                            #linkurl = "http://interfacelift.com/wallpaper/details/"+s[3]+"/"+s[4]+".html"
+                            #os.system('jhead -cl "' + linkurl + '" '+directory+s[3]+"_"+s[4]+"_"+s[5])
+                    else:
+                        os.system('wget -P ' + directory + ' -nc -U "' + useragent + '" ' + 'http://interfacelift.com' + picture)
+                        linkurl = "http://interfacelift.com/wallpaper/details/"+s[3]+"/"+s[4]+".html"
+                        os.system('jhead -cl "' + linkurl + '" '+directory+s[3]+"_"+s[4]+"_"+s[5])
+            if urlcount == 0:
+                    quit()
+            count += 1
+else:
+    print "Usage: interfacelift.py <PathToStoreWallpapers>"
